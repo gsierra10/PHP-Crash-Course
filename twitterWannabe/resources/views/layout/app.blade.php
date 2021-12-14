ca6f3cab22b3fb551797fdadf7571dad390fdc2b
GustavoSierra <gsierra.business@gmail.com>
2021-12-14 01:40:01 +0100

    Added nav bar and styles to it with tailwind

--- twitterWannabe/resources/views/layout/app.blade.php
+++ twitterWannabe/resources/views/layout/app.blade.php
@@ -5,10 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Twitter-Wannabe</title>
-
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body>
+<body class="bg-gray-100">
+    <nav class="p-6 bg-white flex justify-between">
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Post</a>
+            </li>
+        </ul>
+
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Gustavo Sierra</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Login</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Register</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Logout</a>
+            </li>
+        </ul>
+    </nav>
     @yield('content')
 </body>
 </html>

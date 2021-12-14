9c557c63d307b5f112145dbd9bf764dcb27aeae8
GustavoSierra <gsierra.business@gmail.com>
2021-12-14 01:22:43 +0100

    added tailwind css

---
+++ twitterWannabe/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily:{
+      'sans': ['Poppins', 'ui-sans-serif', 'system-ui'], 
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

e66a32716626592ff91f13fc0bc8356b9d5c6ff3
GustavoSierra <gsierra.business@gmail.com>
2021-12-16 00:34:37 +0100

    created login controller and login layout

--- twitterWannabe/routes/web.php
+++ twitterWannabe/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/dashboard',[DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('/login',[LoginController::class, 'index'])->name('login');
+Route::post('/login',[LoginController::class, 'store']);
 
 Route::get('/register',[RegisterController::class, 'index'])->name('register');
 Route::post('/register',[RegisterController::class, 'store']);

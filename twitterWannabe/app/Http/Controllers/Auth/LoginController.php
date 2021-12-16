0f003c42f65c0b3ceab9757f9e51787f1266cee1
GustavoSierra <gsierra.business@gmail.com>
2021-12-16 10:48:50 +0100

    error on the login added

--- twitterWannabe/app/Http/Controllers/Auth/LoginController.php
+++ twitterWannabe/app/Http/Controllers/Auth/LoginController.php
@@ -11,8 +11,17 @@ class LoginController extends Controller
     {
         return view('auth.login');
     }
-    public function store()
+    public function store(Request $request)
     {
-        dd('ok');
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!auth()->attempt($request->only('email', 'password'))) {
+            return back()->with('status', 'Invalid Login Details');
+        }
+
+        return redirect()->route('dashboard');
     }
 }
